34380df73d5b27e6aad1042f81ccad8d80451384
Captain <ogtradewithmeai@gmail.com>
2026-05-24 16:58:33 +0100

    Add tray icon to start.bat

--- start.bat
+++ start.bat
@@ -2,3 +2,4 @@
 cd /d "%~dp0"
 start "LB-App" venv\Scripts\pythonw.exe -m little_brother
 start "LB-Watchdog" venv\Scripts\pythonw.exe tools\watchdog.py
+start "LB-Tray" venv\Scripts\pythonw.exe tools\tray.py
